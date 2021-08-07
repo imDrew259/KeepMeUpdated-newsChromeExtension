17ed763a25096313da95a117745e3f9b65eeef52
imDrew259 <87925003+imDrew259@users.noreply.github.com>
2021-08-07 18:01:44 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,52 @@
+function fetchNews(){
+    const url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=c61e4bf1368248c6ad39d4c2311ae826";
+    return fetch(url)
+        .then(response => {
+            if (response.status === 200) {
+            return response.json()
+            } else {
+            throw new Error('Unable to fetch News');
+            }
+        })
+        .catch(err => {
+            throw err;
+        })
+}
+
+function showNews(dataArray){
+    dataArray.forEach(dataItem=>{
+        let li = document.createElement("li");
+
+        let titleNode = document.createTextNode(dataItem.title);
+        let content = dataItem.description;
+        if(content!=null && content.length > 170) content = content.substr(0,150) + "...";
+        let contentNode = document.createTextNode(content);
+
+        let aTag = document.createElement('a');
+        aTag.setAttribute('href',dataItem.url);
+        aTag.setAttribute('target','_blank');
+        aTag.appendChild(titleNode);
+
+        let img = document.createElement('img');
+        img.setAttribute('src',dataItem.urlToImage);
+
+        let span = document.createElement('SPAN');
+        span.appendChild(contentNode);
+
+        li.appendChild(img);
+        li.appendChild(aTag);
+        li.appendChild(span);
+
+        let  ul = document.getElementById('listItems');
+        ul.appendChild(li);
+    })
+}
+
+window.addEventListener('load',(e)=>{
+    fetchNews().then(data=>{
+        showNews(data.articles);
+    }).catch(err=>{
+         document.getElementById("error").innerHTML = '<span class="material-icons"> error_outline </span>' + err;
+    })
+
+})
